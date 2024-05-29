bf681fec372ebca67a140a53ec8d298a8301da43
mtom94 <meeraktom@gmail.com>
2024-05-30 00:18:49 +0530

    Day_2

--- simple_operations.py
+++ simple_operations.py
@@ -1,3 +1,32 @@
+print("Hello"[0]) #Here the output shows the first character "H"
+print(len("Hello")) #Here the output shows the output of the strind
+print("123" + "456") #shows concatenation like 123456
+
+#Integer
+print(123 + 567)
+
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + "characters")
+
+print(type("What is you name?"))
+
+print(70 + float("100.5"))
+print(str(70) + str(100))
+
+two_digit_number = input("Type a two digit number:")
+print(type(two_digit_number))
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+
+print(first_number)
+print(second_number)
+
+result = int(first_number) + int(second_number)
+print(result)
+
+
+
 print(float(8/3))
 print(type(8/3))
 
--- tipCalculator.py
+++ tipCalculator.py
@@ -2,42 +2,25 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Round the result to 2 decimal places.
 
-# print("Welcome to the tip calculator")
-# bill = float(input("What was the total bill? $"))
-# tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
-# people = int(input("How many people to split the bill?"))
-#
-# tip_as_percent = tip / 100
-# total_tip_amount = bill * tip_as_percent
-# total_bill = bill + total_tip_amount
-# bill_per_person = total_bill /people
-# final_amount = round(bill_per_person,2)
-#
-# print(f"Each person should pay:${final_amount}")
+print("Welcome to the tip calculator!")
 
-# print("Hello"[0]) #Here the output shows the first character "H"
-# print(len("Hello")) #Here the output shows the output of the strind
-# print("123" + "456") #shows concatenation like 123456
-#
-# #Integer
-# print(123 + 567)
+bill = float(input("What was the total bill? $"))
 
-# num_char = len(input("What is your name?"))
-# new_num_char = str(num_char)
-# print("Your name has " + new_num_char + "characters")
-#
-# print(type("What is you name?"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
 
-# print(70 + float("100.5"))
-# print(str(70) + str(100))
+people = int(input("How many people to split the bill?"))
 
-two_digit_number = input("Type a two digit number:")
-print(type(two_digit_number))
-first_number = two_digit_number[0]
-second_number = two_digit_number[1]
+tip_as_percent = tip / 100
 
-print(first_number)
-print(second_number)
+total_tip_amount = bill * tip_as_percent
 
-result = int(first_number) + int(second_number)
-print(result)
+total_bill = bill + total_tip_amount
+
+bill_per_person = total_bill /people
+
+#final_amount = round(bill_per_person,2)
+final_amount = "{:.2f}".format(bill_per_person) #Instead of the round funtion we can use another code of statement to round two decimal places
+
+print(f"Each person should pay:${final_amount}")
+
+print(f"tip {tip_as_percent},total tip amount {total_tip_amount}, total bill {total_bill}, bill per person {bill_per_person}")
