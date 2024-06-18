ef7e70d12c8c718dcc2cd5c8dbcdd8091fdfcb71
mtom94 <meeraktom@gmail.com>
2024-06-18 23:47:11 +0530

    Day_7

--- Hangman_project.py
+++ Hangman_project.py
@@ -11,26 +11,30 @@ print(chosen_word)
 #Create an empty list called display.
 
 display = []
-for "_" in range(chosen_word):
+for _ in range(word_length):
     display += "-"
+print(display)
+
+#or we can use the below code
+
+# display = []
+# for letter in chosen_word:
+#     display += "-"
+# print(display)
 
 #for each letter in the chosen_word, add a "_" to "display".
 #So if the chosen_word was "apple" , display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
-guess = input("Guess a letter:").lower()
-#check guessed letter
-for position in range(word_length):
-    letter = chosen_word[position]
-    print(f"Current position : {position}\n Current letter :{letter}\n Guessed letter:{guess}")
-
+guess = input("Guess a letter:")
 #TODO 2 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 #if the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #eg: If the user guessed "p" and the chosen_word was "apple" , then display should be ["_", "p", "p", "_", "_"].
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
         display[position] = letter
-print(display)
+
 #TODO 3 - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+print(display)
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
 
