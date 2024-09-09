eb0e01fbcdc459399d971bc8c2707dbbc963bc26
mtom94 <meeraktom@gmail.com>
2024-09-09 14:42:24 +0530

    Day_15

--- higer_lower_project.py
+++ higer_lower_project.py
@@ -2,7 +2,6 @@ from higher_lower_art import logo, logo2
 from game_data import higher_lower_data
 import random
 import os
-import platform
 
 def format_data(account):
     """Takes the account data and returns the printable format."""
@@ -15,12 +14,7 @@ def check_answer(guess, a_followers, b_followers):
     """Take the user guess and follower counts and returns if they got it right."""
     return (guess == "a" and a_followers > b_followers) or (guess == "b" and b_followers > a_followers)
 
-def clear_screen():
-    """Clears the screen depending on the OS."""
-    if platform.system() == "Windows":
-        os.system('cls')
-    else:
-        os.system('clear')
+    os.system('clear')
 
 # Display art
 print(logo)
@@ -36,9 +30,9 @@ while game_should_continue:
     while account_a == account_b:
         account_b = random.choice(higher_lower_data)
 
-    print(f"Compare A: {format_data(account_a)}.")
+    print(f"Compare A: {format(account_a)}.")
     print(logo2)
-    print(f"Against B: {format_data(account_b)}.")
+    print(f"Against B: {format(account_b)}.")
 
     # Ask user for a guess
     guess = input("Who has more followers? Type 'A' or 'B': ").lower()
@@ -48,8 +42,6 @@ while game_should_continue:
     b_follower_count = account_b["follower_count"]
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-    # Clear the screen between rounds
-    clear_screen()
     print(logo)
 
     # Give user feedback on their guess and keep score
